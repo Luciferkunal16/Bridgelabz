fa1bb985a2d5aaf98f06ecb958eea699b00cd981
Luciferkunal16 <Kunalbatham15@gmail.com>
2021-10-06 08:33:41 +0530

    Attendence code is updated

--- attendence.sh
+++ attendence.sh
@@ -1,7 +1,15 @@
-x=$((RANDOM%2))
-if [[ $x -eq 1 ]]
-then 
-	echo "Employee is Present"
-else
-	echo "Employee is Absent"
-fi
+function  attendence {
+
+	x=$((RANDOM%2))
+	if [[ $x -eq 1 ]]
+	then 
+		echo "Employee is Present"
+		return 1
+	else
+		echo "Employee is Absent"
+		return 2
+	fi
+}
+
+attendence 
+n=$?
